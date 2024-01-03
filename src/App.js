96ba47d5676e02db36fcb0c3009d6aca7bb97c1c
mortadhaksontini <mortadhaksontini22@gmail.com>
2024-01-03 21:19:36 +0100

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import styled from "styled-components";
 
-function App() {
+const MapContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const App = () => {
+  const mapStyles = {
+    height: "80vh",
+    width: "90%",
+    borderRadius: "10px",
+    border: "1px solid white",
+  };
+
+  const defaultCenter = {
+    lat: 37.7749,
+    lng: 15,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Explore The World <i class="fa-solid fa-earth-africa"></i>
+      </h1>
+      <LoadScript googleMapsApiKey="AIzaSyCvGjwsnln_Mp1XPcIkO1eoNuME4W4KXLE">
+        <MapContainer>
+          <GoogleMap
+            mapContainerStyle={mapStyles}
+            zoom={5}
+            center={defaultCenter}
+          />
+        </MapContainer>
+      </LoadScript>
+    </>
   );
-}
+};
 
 export default App;
