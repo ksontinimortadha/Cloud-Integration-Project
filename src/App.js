007c7db818103896bcfcb51602b120f542603752
mortadhaksontini <mortadhaksontini22@gmail.com>
2024-01-16 10:28:36 +0100

    updating files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
 import styled from "styled-components";
 
+// Styled component for the map container
 const MapContainer = styled.div`
   width: 100%;
   display: flex;
@@ -9,7 +10,9 @@ const MapContainer = styled.div`
   align-items: center;
 `;
 
+// Functional component representing the main App
 const App = () => {
+  // Styles for the map
   const mapStyles = {
     height: "80vh",
     width: "90%",
@@ -17,6 +20,7 @@ const App = () => {
     border: "1px solid white",
   };
 
+  // Default center coordinates for the map
   const defaultCenter = {
     lat: 37.7749,
     lng: 15,
@@ -24,11 +28,16 @@ const App = () => {
 
   return (
     <>
+      {/* Header with a title and an icon */}
       <h1 style={{ color: "white", textAlign: "center" }}>
         Explore The World <i class="fa-solid fa-earth-africa"></i>
       </h1>
+
+      {/* LoadScript component to load the Google Maps API with the provided API key */}
       <LoadScript googleMapsApiKey="AIzaSyCvGjwsnln_Mp1XPcIkO1eoNuME4W4KXLE">
+        {/* Container for the GoogleMap component */}
         <MapContainer>
+          {/* GoogleMap component to display the map */}
           <GoogleMap
             mapContainerStyle={mapStyles}
             zoom={5}
